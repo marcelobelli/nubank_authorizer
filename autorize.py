53f62a14f0c53039c6d6d52c4a5a6eca7a6a1de1
Marcelo Belli <marcelo@belli.me>
2021-10-08 09:55:18 -0300

    Manage the account-not-initialized rule

--- autorize.py
+++ autorize.py
@@ -1,11 +1,10 @@
 import json
-import sys
 
 from copy import deepcopy
 
+
 def input_operation(line):
     return json.loads(line)
-
 
 
 def autorize(data):
@@ -20,18 +19,12 @@ def autorize(data):
                 else:
                     output.append({"account": account_data, "violations": ["account-already-initialized"]})
             case {"transaction": transaction_data}:
-                if transaction_data["amount"] <= account["available-limit"]:
+                if not account:
+                    output.append({"account": deepcopy(account), "violations": ["account-not-initialized"]})
+                elif transaction_data["amount"] <= account["available-limit"]:
                     account["available-limit"] -= transaction_data["amount"]
                     output.append({"account": deepcopy(account), "violations": []})
                 else:
                     output.append({"account": deepcopy(account), "violations": ["insufficient-limit"]})
-                
-    
+
     return output
-
-
-
-
-            
-
-    
--- test_autorize.py
+++ test_autorize.py
@@ -1,5 +1,4 @@
 import io
-import json
 import sys
 
 import autorize
@@ -55,18 +54,75 @@ def test_account_already_initialized_rule():
 
     assert autorize.autorize(input) == expected_output
 
+
 def test_insufficient_limit_rule():
     input = [
         {"account": {"active-card": True, "available-limit": 20}},
-        {"transaction": {"merchant": "Burger King", "amount": 10, "time": "2019-02-13T10:00:00.000Z"}},
-        {"transaction": {"merchant": "Habbib's", "amount": 20, "time": "2019-02-13T11:00:00.000Z"}},
-        {"transaction": {"merchant": "Uber", "amount": 5, "time": "2019-02-13T12:00:00.000Z"}},
+        {
+            "transaction": {
+                "merchant": "Burger King",
+                "amount": 10,
+                "time": "2019-02-13T10:00:00.000Z",
+            }
+        },
+        {
+            "transaction": {
+                "merchant": "Habbib's",
+                "amount": 20,
+                "time": "2019-02-13T11:00:00.000Z",
+            }
+        },
+        {
+            "transaction": {
+                "merchant": "Uber",
+                "amount": 5,
+                "time": "2019-02-13T12:00:00.000Z",
+            }
+        },
     ]
     expected_output = [
         {"account": {"active-card": True, "available-limit": 20}, "violations": []},
         {"account": {"active-card": True, "available-limit": 10}, "violations": []},
-        {"account": {"active-card": True, "available-limit": 10}, "violations": ["insufficient-limit"]},
+        {
+            "account": {"active-card": True, "available-limit": 10},
+            "violations": ["insufficient-limit"],
+        },
         {"account": {"active-card": True, "available-limit": 5}, "violations": []},
+    ]
+
+    assert autorize.autorize(input) == expected_output
+
+
+def test_account_not_initialized_rule():
+    input = [
+        {
+            "transaction": {
+                "merchant": "Burger King",
+                "amount": 10,
+                "time": "2019-02-13T10:00:00.000Z",
+            }
+        },
+        {
+            "transaction": {
+                "merchant": "Habbib's",
+                "amount": 20,
+                "time": "2019-02-13T11:00:00.000Z",
+            }
+        },
+        {"account": {"active-card": True, "available-limit": 100}},
+        {
+            "transaction": {
+                "merchant": "Uber",
+                "amount": 5,
+                "time": "2019-02-13T12:00:00.000Z",
+            }
+        },
+    ]
+    expected_output = [
+        {"account": {}, "violations": ["account-not-initialized"]},
+        {"account": {}, "violations": ["account-not-initialized"]},
+        {"account": {"active-card": True, "available-limit": 100}, "violations": []},
+        {"account": {"active-card": True, "available-limit": 95}, "violations": []},
     ]
 
     assert autorize.autorize(input) == expected_output
