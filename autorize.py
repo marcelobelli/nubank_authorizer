7fe1e467a0c851e325bf18d98d688ebb9f4b0e45
Marcelo Belli <marcelo@belli.me>
2021-10-07 22:23:50 -0300

    Manage the account-already-initialized and the insufficient-limit rules

--- autorize.py
+++ autorize.py
@@ -1,7 +1,37 @@
 import json
 import sys
 
+from copy import deepcopy
 
 def input_operation(line):
     return json.loads(line)
 
+
+
+def autorize(data):
+    account = {}
+    output = []
+    for line in data:
+        match line:
+            case {"account": account_data}:
+                if not account:
+                    account = deepcopy(account_data)
+                    output.append({"account": account_data, "violations": []})
+                else:
+                    output.append({"account": account_data, "violations": ["account-already-initialized"]})
+            case {"transaction": transaction_data}:
+                if transaction_data["amount"] <= account["available-limit"]:
+                    account["available-limit"] -= transaction_data["amount"]
+                    output.append({"account": deepcopy(account), "violations": []})
+                else:
+                    output.append({"account": deepcopy(account), "violations": ["insufficient-limit"]})
+                
+    
+    return output
+
+
+
+
+            
+
+    
--- test_autorize.py
+++ test_autorize.py
@@ -38,3 +38,35 @@ def test_input_operations(monkeypatch):
     output = [autorize.input_operation(line) for line in sys.stdin]
 
     assert output == expected_output
+
+
+def test_account_already_initialized_rule():
+    input = [
+        {"account": {"active-card": True, "available-limit": 175}},
+        {"account": {"active-card": True, "available-limit": 350}},
+    ]
+    expected_output = [
+        {"account": {"active-card": True, "available-limit": 175}, "violations": []},
+        {
+            "account": {"active-card": True, "available-limit": 350},
+            "violations": ["account-already-initialized"],
+        },
+    ]
+
+    assert autorize.autorize(input) == expected_output
+
+def test_insufficient_limit_rule():
+    input = [
+        {"account": {"active-card": True, "available-limit": 20}},
+        {"transaction": {"merchant": "Burger King", "amount": 10, "time": "2019-02-13T10:00:00.000Z"}},
+        {"transaction": {"merchant": "Habbib's", "amount": 20, "time": "2019-02-13T11:00:00.000Z"}},
+        {"transaction": {"merchant": "Uber", "amount": 5, "time": "2019-02-13T12:00:00.000Z"}},
+    ]
+    expected_output = [
+        {"account": {"active-card": True, "available-limit": 20}, "violations": []},
+        {"account": {"active-card": True, "available-limit": 10}, "violations": []},
+        {"account": {"active-card": True, "available-limit": 10}, "violations": ["insufficient-limit"]},
+        {"account": {"active-card": True, "available-limit": 5}, "violations": []},
+    ]
+
+    assert autorize.autorize(input) == expected_output
