d6d284bb5c8b8f6ccde89d254a003fef29cb1ca2
Marcelo Belli <marcelo@belli.me>
2021-10-09 20:59:16 -0300

    Refactor process_high_frequency_small_interval_rule

--- sliding_window.py
+++ sliding_window.py
@@ -5,7 +5,7 @@ from pydantic import BaseModel
 class HighFrequencySlidingWindow(BaseModel):
     transactions: list[dict] = []
     time_window_in_secs: int = 120
-    max_transactions_in_time_window: int = 3
+    max_transactions_permitted: int = 3
 
     @property
     def first_transaction_dt(self):
@@ -15,25 +15,24 @@ class HighFrequencySlidingWindow(BaseModel):
     def successful_transactions(self):
         return len(self.transactions)
 
-    def process_high_frequency_small_interval_rule(self, next_transaction):
-        next_transaction_dt = pendulum.parse(next_transaction["time"])
+    def process_high_frequency_small_interval_rule(self, transaction):
+        next_transaction_dt = pendulum.parse(transaction["time"])
+
         if not self.transactions:
-            self.transactions.append(next_transaction)
+            self.transactions.append(transaction)
             return True
 
         for _ in range(len(self.transactions)):
-            time_window_in_secs_diff = next_transaction_dt.float_timestamp - self.first_transaction_dt.float_timestamp
-            if time_window_in_secs_diff > self.time_window_in_secs:
-                if not self.transactions:
-                    self.transactions.append(next_transaction)
-                    break
-                del self.transactions[0]
-            else:
-                if len(self.transactions) == self.max_transactions_in_time_window:
-                    return False
-                self.transactions.append(next_transaction)
-                break
-        else:
-            self.transactions.append(next_transaction)
+            time_window_diff = next_transaction_dt.float_timestamp - self.first_transaction_dt.float_timestamp
 
+            if time_window_diff < self.time_window_in_secs and self.successful_transactions == self.max_transactions_permitted:
+                return False
+
+            if time_window_diff < self.time_window_in_secs:
+                self.transactions.append(transaction)
+                return True
+
+            del self.transactions[0]
+
+        self.transactions.append(transaction)
         return True
