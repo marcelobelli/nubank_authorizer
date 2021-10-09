e0b1f777c0e69b9a03fa9ca998bde966bc392ccf
Marcelo Belli <marcelo@belli.me>
2021-10-09 20:57:49 -0300

    Create more tests for HighFrequencySlidingWindow

--- test_sliding_window.py
+++ test_sliding_window.py
@@ -177,3 +177,97 @@ def test_three_successful_and_two_denied_transactions_in_less_than_time_window()
     assert sliding_window.transactions[0] == first_transaction["transaction"]
     assert sliding_window.transactions[1] == second_transaction["transaction"]
     assert sliding_window.transactions[2] == third_transaction["transaction"]
+
+
+def test_three_successful_and_one_denied_in_less_than_time_window_then_one_successful_after():
+    sliding_window = HighFrequencySlidingWindow()
+    first_transaction = {"transaction": {"merchant": "Burger King", "amount": 20, "time": "2019-02-13T11:00:00.000Z"}}
+    second_transaction = {"transaction": {"merchant": "Habbib's", "amount": 20, "time": "2019-02-13T11:00:40.000Z"}}
+    third_transaction = {"transaction": {"merchant": "McDonald's", "amount": 20, "time": "2019-02-13T11:01:01.000Z"}}
+    fourth_transaction = {"transaction": {"merchant": "Subway", "amount": 20, "time": "2019-02-13T11:01:31.000Z"}}
+    fifth_transaction = {"transaction": {"merchant": "Burger King", "amount": 30, "time": "2019-02-13T11:02:30.000Z"}}
+
+    first_response = sliding_window.process_high_frequency_small_interval_rule(first_transaction["transaction"])
+    second_response = sliding_window.process_high_frequency_small_interval_rule(second_transaction["transaction"])
+    third_response = sliding_window.process_high_frequency_small_interval_rule(third_transaction["transaction"])
+    fourth_response = sliding_window.process_high_frequency_small_interval_rule(fourth_transaction["transaction"])
+    fifth_response = sliding_window.process_high_frequency_small_interval_rule(fifth_transaction["transaction"])
+
+    assert first_response is True
+    assert second_response is True
+    assert third_response is True
+    assert fourth_response is False
+    assert fifth_response is True
+
+    assert sliding_window.first_transaction_dt == pendulum.parse("2019-02-13T11:00:40.000Z")
+    assert sliding_window.successful_transactions == 3
+    assert len(sliding_window.transactions) == 3
+    assert sliding_window.transactions[0] == second_transaction["transaction"]
+    assert sliding_window.transactions[1] == third_transaction["transaction"]
+    assert sliding_window.transactions[2] == fifth_transaction["transaction"]
+
+
+def test_three_successful_and_one_denied_in_less_than_time_window_then_one_successful_and_one_denied_after():
+    sliding_window = HighFrequencySlidingWindow()
+    first_transaction = {"transaction": {"merchant": "Burger King", "amount": 20, "time": "2019-02-13T11:00:00.000Z"}}
+    second_transaction = {"transaction": {"merchant": "Habbib's", "amount": 20, "time": "2019-02-13T11:00:40.000Z"}}
+    third_transaction = {"transaction": {"merchant": "McDonald's", "amount": 20, "time": "2019-02-13T11:01:01.000Z"}}
+    fourth_transaction = {"transaction": {"merchant": "Subway", "amount": 20, "time": "2019-02-13T11:01:31.000Z"}}
+    fifth_transaction = {"transaction": {"merchant": "Burger King", "amount": 30, "time": "2019-02-13T11:02:30.000Z"}}
+    sixth_transaction = {"transaction": {"merchant": "Habbib's", "amount": 20, "time": "2019-02-13T11:02:35.000Z"}}
+
+    first_response = sliding_window.process_high_frequency_small_interval_rule(first_transaction["transaction"])
+    second_response = sliding_window.process_high_frequency_small_interval_rule(second_transaction["transaction"])
+    third_response = sliding_window.process_high_frequency_small_interval_rule(third_transaction["transaction"])
+    fourth_response = sliding_window.process_high_frequency_small_interval_rule(fourth_transaction["transaction"])
+    fifth_response = sliding_window.process_high_frequency_small_interval_rule(fifth_transaction["transaction"])
+    sixth_response = sliding_window.process_high_frequency_small_interval_rule(sixth_transaction["transaction"])
+
+    assert first_response is True
+    assert second_response is True
+    assert third_response is True
+    assert fourth_response is False
+    assert fifth_response is True
+    assert sixth_response is False
+
+    assert sliding_window.first_transaction_dt == pendulum.parse("2019-02-13T11:00:40.000Z")
+    assert sliding_window.successful_transactions == 3
+    assert len(sliding_window.transactions) == 3
+    assert sliding_window.transactions[0] == second_transaction["transaction"]
+    assert sliding_window.transactions[1] == third_transaction["transaction"]
+    assert sliding_window.transactions[2] == fifth_transaction["transaction"]
+
+
+def test_three_successful_and_one_denied_in_less_than_time_window_then_three_successful_after():
+    sliding_window = HighFrequencySlidingWindow()
+    first_transaction = {"transaction": {"merchant": "Burger King", "amount": 20, "time": "2019-02-13T11:00:00.000Z"}}
+    second_transaction = {"transaction": {"merchant": "Habbib's", "amount": 20, "time": "2019-02-13T11:00:40.000Z"}}
+    third_transaction = {"transaction": {"merchant": "McDonald's", "amount": 20, "time": "2019-02-13T11:01:01.000Z"}}
+    fourth_transaction = {"transaction": {"merchant": "Subway", "amount": 20, "time": "2019-02-13T11:01:31.000Z"}}
+    fifth_transaction = {"transaction": {"merchant": "Burger King", "amount": 30, "time": "2019-02-13T11:03:02.000Z"}}
+    sixth_transaction = {"transaction": {"merchant": "Habbib's", "amount": 20, "time": "2019-02-13T11:03:03.000Z"}}
+    seventh_transaction = {"transaction": {"merchant": "McDonald's", "amount": 20, "time": "2019-02-13T11:03:04.000Z"}}
+
+    first_response = sliding_window.process_high_frequency_small_interval_rule(first_transaction["transaction"])
+    second_response = sliding_window.process_high_frequency_small_interval_rule(second_transaction["transaction"])
+    third_response = sliding_window.process_high_frequency_small_interval_rule(third_transaction["transaction"])
+    fourth_response = sliding_window.process_high_frequency_small_interval_rule(fourth_transaction["transaction"])
+    fifth_response = sliding_window.process_high_frequency_small_interval_rule(fifth_transaction["transaction"])
+    sixth_response = sliding_window.process_high_frequency_small_interval_rule(sixth_transaction["transaction"])
+    seventh_response = sliding_window.process_high_frequency_small_interval_rule(seventh_transaction["transaction"])
+
+    assert first_response is True
+    assert second_response is True
+    assert third_response is True
+    assert fourth_response is False
+    assert fifth_response is True
+    assert sixth_response is True
+    assert seventh_response is True
+
+    assert sliding_window.first_transaction_dt == pendulum.parse("2019-02-13T11:03:02.000Z")
+    assert sliding_window.successful_transactions == 3
+    assert len(sliding_window.transactions) == 3
+    assert sliding_window.transactions[0] == fifth_transaction["transaction"]
+    assert sliding_window.transactions[1] == sixth_transaction["transaction"]
+    assert sliding_window.transactions[2] == seventh_transaction["transaction"]
+
