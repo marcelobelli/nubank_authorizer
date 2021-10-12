d45ed5ce8a9fc386795505e97a98b1ddd1656cd6
Marcelo Belli <marcelo@belli.me>
2021-10-12 20:26:21 -0300

    Move transactions rules to a separate namespace and refactor authorize

--- authorize.py
+++ authorize.py
@@ -3,7 +3,7 @@ import json
 from copy import deepcopy
 from processors import process_frequency_transaction, process_repeated_transaction
 from state import AccountState
-
+from rules import transaction_rules
 
 def input_operation(line):
     return json.loads(line)
@@ -13,42 +13,7 @@ def _generate_single_output(account, violations):
     return {"account": deepcopy(account), "violations": violations}
 
 
-def high_frequency_rule(account_state: AccountState, transaction: dict, violations: list) -> tuple[AccountState, list]:
-    account_state, result = process_frequency_transaction(account_state, transaction)
-    if account_state.account_initialized and result is False:
-        violations.append("high-frequency-small-interval")
 
-    return account_state, violations
-
-
-def doubled_transaction_rule(
-        account_state: AccountState, transaction: dict, violations: list
-) -> tuple[AccountState, list]:
-    account_state, result = process_repeated_transaction(account_state, transaction)
-    if account_state.account_initialized and result is False:
-        violations.append("doubled-transaction")
-
-    return account_state, violations
-
-def card_not_active_rule(account_state: AccountState, transaction: dict, violations: list) -> tuple[AccountState, list]:
-    if account_state.account_initialized and account_state.active_card is False:
-        violations.append("card-not-active")
-
-    return account_state, violations
-
-
-def insufficient_limit_rule(account_state: AccountState, transaction: dict, violations: list) -> tuple[AccountState, list]:
-    if account_state.account_initialized and transaction["amount"] > account_state.available_limit:
-        violations.append("insufficient-limit")
-
-    return account_state, violations
-
-
-def account_initialized_rule(account_state: AccountState, transaction: dict, violations: list) -> tuple[AccountState, list]:
-    if not account_state.account_initialized:
-        violations.append("account-not-initialized")
-
-    return account_state, violations
 
 
 def authorize(data):
@@ -64,11 +29,8 @@ def authorize(data):
                     output.append(_generate_single_output(account_state.to_dict(), ["account-already-initialized"]))
             case {"transaction": transaction_data}:
                 violations = []
-                account_state, violations = account_initialized_rule(account_state, transaction_data, violations)
-                account_state, violations = card_not_active_rule(account_state, transaction_data, violations)
-                account_state, violations = insufficient_limit_rule(account_state, transaction_data, violations)
-                account_state, violations = high_frequency_rule(account_state, transaction_data, violations)
-                account_state, violations = doubled_transaction_rule(account_state, transaction_data, violations)
+                for rule in transaction_rules:
+                    account_state, violations = rule(account_state, transaction_data, violations)
 
                 if not violations:
                     account_state.available_limit -= transaction_data["amount"]
---
+++ rules.py
@@ -0,0 +1,49 @@
+from processors import process_frequency_transaction, process_repeated_transaction
+from state import AccountState
+
+
+def high_frequency_rule(account_state: AccountState, transaction: dict, violations: list) -> tuple[AccountState, list]:
+    account_state, result = process_frequency_transaction(account_state, transaction)
+    if account_state.account_initialized and result is False:
+        violations.append("high-frequency-small-interval")
+    return account_state, violations
+
+
+def doubled_transaction_rule(
+    account_state: AccountState, transaction: dict, violations: list
+) -> tuple[AccountState, list]:
+    account_state, result = process_repeated_transaction(account_state, transaction)
+    if account_state.account_initialized and result is False:
+        violations.append("doubled-transaction")
+    return account_state, violations
+
+
+def card_not_active_rule(account_state: AccountState, transaction: dict, violations: list) -> tuple[AccountState, list]:
+    if account_state.account_initialized and account_state.active_card is False:
+        violations.append("card-not-active")
+    return account_state, violations
+
+
+def insufficient_limit_rule(
+    account_state: AccountState, transaction: dict, violations: list
+) -> tuple[AccountState, list]:
+    if account_state.account_initialized and transaction["amount"] > account_state.available_limit:
+        violations.append("insufficient-limit")
+    return account_state, violations
+
+
+def account_initialized_rule(
+    account_state: AccountState, transaction: dict, violations: list
+) -> tuple[AccountState, list]:
+    if not account_state.account_initialized:
+        violations.append("account-not-initialized")
+    return account_state, violations
+
+
+transaction_rules = [
+    account_initialized_rule,
+    card_not_active_rule,
+    insufficient_limit_rule,
+    high_frequency_rule,
+    doubled_transaction_rule,
+]
